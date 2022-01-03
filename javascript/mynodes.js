7a1a0e39f3c78b34b35a9ec2bae63173726071ba
tahirbey <50322297+tahirbey@users.noreply.github.com>
2022-01-03 15:55:23 +0300

    Add files via upload

--- javascript/mynodes.js
+++ javascript/mynodes.js
@@ -4,3 +4,21 @@ export class Node {
       this.next = null;
     }
   }
+
+
+export class DNode {
+  constructor(value) {
+    this.value = value
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+
+export class TreeNode{
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
