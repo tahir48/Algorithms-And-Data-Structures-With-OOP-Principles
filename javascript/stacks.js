9af3058e5c967e6026fee91f3139a72834783b8f
tahirbey <50322297+tahirbey@users.noreply.github.com>
2022-01-03 15:14:38 +0300

    Add files via upload

---
+++ javascript/stacks.js
@@ -0,0 +1,45 @@
+import { Node } from "./mynodes.js";
+
+
+export class Stack {
+    constructor(value){
+        newnode = new Node(value);
+        this.top = newnode
+        this.height = 1
+    }
+
+
+    print_stack(){
+        if (this.top == null){
+            return
+        }
+        temp = this.top
+        while (temp != null){
+            console.log(temp.value)
+            temp = temp.next
+        }
+    }
+
+
+    push(value) {
+        var newnode = new Node(value);
+        if (this.top == null) {
+            this.top = newnode;
+            return
+        }
+        newnode.next = this.top
+        this.top = newnode
+        this.height += 1
+    }
+
+    pop() {
+        if (this.height == 0) {
+            return
+        }
+        temp = this.top.next;
+        this.top.next = null;
+        this.top = temp;
+        this.height -= 1
+    }
+
+}
