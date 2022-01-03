9afd4dda92462df01a8df7572f8e7de3e9ae6ff3
tahirbey <50322297+tahirbey@users.noreply.github.com>
2022-01-03 15:30:42 +0300

    Add files via upload

---
+++ javascript/queues.js
@@ -0,0 +1,62 @@
+import { Node } from "./mynodes.js";
+
+
+export class Queue {
+
+
+    constructor(value){
+        var newnode = new Node(value);
+        this.front = newnode;
+        this.rear = newnode;
+        this.length = 1;
+    }
+
+
+    print_queue(){
+        if (this.front == null) {
+            return null
+        }
+
+        temp = this.front;
+        while(temp != null) {
+            console.log(temp.value);
+            temp = temp.next
+        }
+    }
+
+    enqueue(value){
+        var newnode = new Node(value);
+        if (this.length == 0) {
+            this.rear = newnode;
+            this.front = newnode;
+        } else {
+            this.rear.next = newnode;
+            this.rear = newnode;
+        }
+        this.length += 1
+    }
+    
+
+    dequeue(){
+        if(this.length == 0) {
+            return
+        }
+        temp = this.front;
+        if(this.length == 1) {
+            this.rear = null;
+            this.front = null;
+        } else {
+            this.front = this.front.next;
+            temp.next = null;
+        }
+        this.length -= 1
+    }
+}
+
+
+        
+            
+//         else:
+//             self.front = self.front.next
+//             temp.next = None
+//         self.length -= 1
