88939d9652e47f95f768dc24e6b470f9d2df90f5
tahirbey <50322297+tahirbey@users.noreply.github.com>
2022-01-07 16:59:24 +0300

    Update MyTreeNode.py

--- Python/MyTreeNode.py
+++ Python/MyTreeNode.py
@@ -1,15 +1,18 @@
 class TreeNode:
 
-    def __init__(self,value):
+    
+    def __init__(self, value):
         self._value = value
         self.right = None
         self.left = None
+    
     
     @property
     def value(self):
         return self.value
 
+    
     @value.setter
-    def value(self,newvalue):
+    def value(self, newvalue):
         self._value = newvalue
 
