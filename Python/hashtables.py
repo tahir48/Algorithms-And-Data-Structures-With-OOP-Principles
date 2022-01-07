e7671713f6cfd94674242498391c2484c011f1aa
tahirbey <50322297+tahirbey@users.noreply.github.com>
2022-01-07 16:55:27 +0300

    Update hashtables.py

--- Python/hashtables.py
+++ Python/hashtables.py
@@ -1,28 +1,31 @@
 class HashTable:
 
+    
     def __init__(self, length):
         self.length = length
         self.Table = [None] * length
         
         
-    def myhasher(self,key):
+    def my_hasher(self, key):
         dummy = 0
         for i in key:
             dummy = (dummy + (ord(i) * 47)) % self.length
         return dummy
 
+    
     def print_table(self):
         for i,j in enumerate(self.Table):
             print(i,j)
 
-    def set_item(self,key,value):
+            
+    def set_item(self, key, value):
         index = self.myhasher(key)
         if self.Table[index] is None:
             self.Table[index] = []
         self.Table[index].append([key,value])
 
 
-    def get_item(self,key):
+    def get_item(self, key):
         index = self.myhasher(key)
         if self.Table[index] is None:
             return False
