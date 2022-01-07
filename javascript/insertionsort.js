891652b6588a8976fad5a00bd28d45a9136d2e07
tahirbey <50322297+tahirbey@users.noreply.github.com>
2022-01-07 18:29:05 +0300

    Update insertionsort.js

--- javascript/insertionsort.js
+++ javascript/insertionsort.js
@@ -20,25 +20,3 @@ export function insertion_sort(unsorted_list){
 return unsorted_list
 }
 
-
-
-
-
-
-// from bubblesort import switch
-
-
-// def insertion_sort(unsorted_list):
-//     for i in range(1,len(unsorted_list)): # index of value that will be shifted to 
-//         t = i
-//         j = i-1
-//         temp = unsorted_list[i]
-//         while temp < unsorted_list[j] and j > -1:
-//             if unsorted_list[j] > temp:
-//                 switch(unsorted_list,t,j)
-//                 j -= 1
-//                 t -= 1
-//     return unsorted_list
-
-
-// print(insertion_sort([2,4,1,6,3,5,9,7,8]))
