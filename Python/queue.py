783876aaa5a1c1fec40effb23f284fdb182b9363
tahirbey <50322297+tahirbey@users.noreply.github.com>
2022-01-07 16:58:39 +0300

    Update queue.py

--- Python/queue.py
+++ Python/queue.py
@@ -4,11 +4,13 @@ from mynode import Node
 
 class Queue:
     
-    def __init__(self,value):
+    
+    def __init__(self, value):
         newnode = Node(value)
         self.front = newnode
         self.rear = newnode
         self.length = 1
+        
         
     def print_queue(self):
         if self.front is None:
@@ -18,7 +20,8 @@ class Queue:
             print(temp.value)
             temp = temp.next 
     
-    def enqueue(self,value):
+    
+    def enqueue(self, value):
         newnode = Node(value)
         if self.length == 0:
             self.rear = newnode
@@ -27,8 +30,7 @@ class Queue:
             self.rear.next = newnode
             self.rear = newnode   
         self.length += 1
-        
-            
+                 
     
     def dequeue(self):
         if self.length == 0:
@@ -43,15 +45,11 @@ class Queue:
         self.length -= 1
         
         
-    
-    
 myqueue = Queue(0)
 myqueue.enqueue(1)
 myqueue.enqueue(2)
 myqueue.enqueue(3)
 myqueue.enqueue(4)
 #myqueue.dequeue()
-
-
 #myqueue.dequeue()
 myqueue.print_queue()
